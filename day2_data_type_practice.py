6ecbe813d8577076c9cdc0c5ece5adeb81ce2a35
gsangeetha-yadav <gsangeethasgp@gmail.com>
2026-02-25 19:08:49 +0530

    Day 2 : data types practice program

---
+++ day2_data_type_practice.py
@@ -0,0 +1,24 @@
+#Taking student details
+
+student_name = input("Enter student name:")
+age = int(input("Enter student age:"))
+height = float(input("Enter student height in meters:")
+passed = input("Enter pass status (True/False):")
+
+#printing Values
+
+print(Student_name)
+print(age)
+print(height)
+print(passed)
+
+#printing data types
+
+print(type(student_name))
+print(type(age))
+print(type(height))
+print(type(passed))
+
+#printing summary
+
+print(f"Student {student_name} is {age} years old,height is {height}m and pass status is {passed}.")
