3b6b85daa6d8df30c7c773f5e2a98fd546344704
gsangeetha-yadav <gsangeethasgp@gmail.com>
2026-03-06 21:57:51 +0530

    Add python program for basic list operations

---
+++ day11_list_methods_practice.py
@@ -0,0 +1,14 @@
+#program to perform basic list operations using append , remove ,insert ,pop and len methods
+
+
+numbers = [10,20,30,40,50]
+numbers.append(25)
+print(numbers)
+numbers.remove(20)
+print(numbers)
+numbers.insert(1,35)
+print(numbers)
+numbers.pop()
+print(numbers)
+print(len(numbers))
+print(numbers)
