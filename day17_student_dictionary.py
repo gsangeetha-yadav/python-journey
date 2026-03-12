f4a69be65d735b61f9f93cdc0daa30694e8b3b14
gsangeetha-yadav <gsangeethasgp@gmail.com>
2026-03-12 22:14:40 +0530

    Add files via upload
    Day 17 : Added a python program to store and display student details using dictionary

---
+++ day17_student_dictionary.py
@@ -0,0 +1,9 @@
+# python program to store student details in a dictionary and print all key - value pairs using a for loop
+
+student = {
+        "name":"sangeetha",
+        "age":18,
+        "course":"python"
+}
+for k,v in student.items():
+    print(k,v)
