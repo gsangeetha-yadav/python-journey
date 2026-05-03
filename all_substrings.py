03dd63a535f6185ad458c911e4a81580a99f1b9e
gsangeetha-yadav <gsangeethasgp@gmail.com>
2026-05-03 21:07:59 +0530

    Create all_substrings.py
    Added program to generate all substrings of a string using nested loops

---
+++ all_substrings.py
@@ -0,0 +1,9 @@
+# Program to print all substrings of a string
+
+string = input("Enter a string: ")
+
+n = len(string)
+
+for i in range(n):
+    for j in range(i + 1, n + 1):
+        print(string[i:j])
